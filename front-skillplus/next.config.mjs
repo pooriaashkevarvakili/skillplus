2da60043db3edd7006ed2177a7b7e3e3dc70316a
pooriavakili09 <pooriavakili09@gmail.com>
2025-02-15 20:36:42 +0330

    update

--- front-skillplus/next.config.mjs
+++ front-skillplus/next.config.mjs
@@ -2,7 +2,7 @@ import withPWA from 'next-pwa';
 
 const nextConfig = {
 
-//output: 'export',
+output: 'export',
   reactStrictMode: true,
   compress: false,
   images: {
--- skill-plus-back/src/main.ts
+++ skill-plus-back/src/main.ts
@@ -7,7 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+    const allowedOrigins = ['http://localhost:5000', 'http://skillplus.surge.sh'];
+    const origin = req.headers.origin;
+  
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-type, Accept');
     next();
